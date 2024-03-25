a98bd26f82f603444e1053370dd2381136cc0133
seraphicalness <128676942+seraphicalness@users.noreply.github.com>
2024-03-25 13:44:32 +0900

    [level 0] Title: 문자열 돌리기, Time: 32.46 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        
+        foreach(char i in s){
+            Console.WriteLine(i);
+        }
+        
+        // for (int i=0; i<s.Length; i++){
+        //     Console.System.WriteLine(s[i]);
+        // }
+
+    }
+}
