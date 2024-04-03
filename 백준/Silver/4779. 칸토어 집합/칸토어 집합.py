7a4e3ff6f03667b8323b2edcebba2d2bb1e66646
seraphicalness <128676942+seraphicalness@users.noreply.github.com>
2024-04-03 20:44:32 +0900

    [Silver III] Title: 칸토어 집합, Time: 544 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,16 +1,16 @@
 def cantor(n):
-    if n == 0 :
-        print("-", end="")
+    if n == 0 : # n이 0이 될 때
+        print("-", end="") # 줄바꿈하지않고 -찍기
         return
     
-    cantor(n-1)
+    cantor(n-1) # 재귀함수 앞에 선 부분 호출해서 반복
 
-    for i in range(3**(n-1)):
+    for i in range(3**(n-1)): # 3의 제곱 수 만큼 공백 찍기
         print(" ", end="")
     
-    cantor(n-1)
+    cantor(n-1) # 재귀함수 뒤에 선 부분 호출해서 반복
 
-while True:
+while True: # 여러번 입력받기 
     try:
         n = int(input())
         cantor(n)
