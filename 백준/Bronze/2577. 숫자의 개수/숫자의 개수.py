9e5eaa4424b18af4514e9e99ae5ceec16d7cc640
seraphicalness <128676942+seraphicalness@users.noreply.github.com>
2024-05-13 20:55:54 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -3,12 +3,12 @@ B = int(input())
 C = int(input())
 
 total = A * B * C
-result = []
-while total > 0:
+result = [] 
+while total > 0: # result에 total을 분해해서 들어가게 반복, 0이상일때만 
     temp = total % 10
     result.insert(0, temp)
     total //= 10 
     
-for i in range(10):
-    a = result.count(i)
+for i in range(10): # 0부터 9까지 
+    a = result.count(i) # 원소 카운트해서 출력 
     print(a, end="\n")
